b29d03691332b81103c53e0d08e9aa1cd89121a0
CauaSDO <cauasilva@icloud.com>
2022-01-04 19:09:14 -0300

    AULA 2 / TIPOS E CONSTANTES

---
+++ CSO06-Swift2-constantes e tipos.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//CONSTANTES
+let pi = 3.1415926  //Unmutable
+let monthsInYear = 12
+let Pi = 3.14
+
+
+
+//TIPOS
+//Bool, Int, Float, Double, Character e String
+
+//Booleanos (Bool)
+var isFirstTime: Bool = true
+var likesFruits = true
+
+//Inteiros (Int) SIGNED OU UNSIGNED
+var age: Int = 25
+var newAge = 43
+
+//Float -> 32Bit
+var dolar: Float = 5.1
+
+//Double -> 64Bit
+var crazyNumber = 1000.78690
+
+//Character: 1 caracter
+var gender: Character = "M" //"F"
+var enter: Character = "\n"
+var aspas: Character = "\""
+
+//String: Conjunto de caracteres
+var newGender = "M"
+let studentName = "\"Dr.\" Paulo Henrique Bastos"
+print(studentName)
+let firstName = "Cauã"
+let lastName = "Silva"
+let fullName = firstName + " " + lastName
+print("A varariavel fullName tem \(fullName.count) caracteres")
+print(fullName)
+let next = "Olá, espero que esteja gostando do curso.\nAinda tem muito mais pela frente"
+print(next)
+
+
+
+// 1KB 1.000 Byte
+// 1MB 1.000.000 Byte
+// 1GB 1.000.000.000 Byte
+// 1TB 1.000.000.000.000 Byte
